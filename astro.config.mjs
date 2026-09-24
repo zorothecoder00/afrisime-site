5a030bb33d222a6bf68a0ca858682413b631de77
zorothecoder00 <awledouamoussou95@gmail.com>
2026-09-24 15:40:51 +0000

    Initialisation du site vitrine Astro

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  site: "https://votre-domaine.com"
+
+  integrations: [sitemap()]
+});
